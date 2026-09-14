5092e4f4606921fcbee7049067f9a14db4aca86f
okylearn-blip <okylearn@gmail.com>
2026-09-14 21:35:07 +0800

    music

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ const CONFIG = {
   // ── Recipient Info ────────────────────────────────────────────
   name: "Pitii kimpulll",
   photo: "./img/irene.jpg",       // Place your photo in the img/ folder
-  music: "./music/hbd.mpeg",      // Place your music in the music/ folder
+  music: "./music/About you.mp3",      // Place your music in the music/ folder
 
   // ── Theme Colors ──────────────────────────────────────────────
   // A toggle button lets the viewer switch between dark & light mode.
